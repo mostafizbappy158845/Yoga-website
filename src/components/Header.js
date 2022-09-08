1808c76b63e106aedb6281517369246aab50f780
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2022-09-09 03:55:01 +0600

    nav and mobileNav done

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,31 @@ const Header = () => {
   const [header, setHeader] = useState(false);
 
   return ( 
-    <header className={`${header ? 'top-0' : 'top-9'} fixed bg-pink-200 w-full
-     max-w-[90vw] lg:max-w-[1170px] mx-auto rounded-md h-[90px] shadow-primary px-4 lg:px-8 z-20 transition-all duration-500`}>
-      <div className="flex"></div>
+    <header className={`${header ? 'top-0' : 'top-9'} fixed bg-white w-full
+     max-w-[90vw] lg:max-w-[1170px] mx-auto rounded-md h-[90px] shadow-primary px-4 lg:px-8 z-20 transition-all duration-500 flex items-center justify-between`}>
+      <div className="flex items-center">
+        {/* Logo */}
+        <a href="#">
+          <img src={Logo} alt="" />
+        </a>
+        {/* nav */}
+        <div className='hidden lg:flex'>
+          <Nav/>
+        </div>
+      </div>
+      <div className='flex items-center '>
+        {/* button */}
+        <div className='flex gap-x-4 lg:gap-x-9 '>
+          <button className='text-heading font-medium text-sm lg:text-base hover:text-orange transition'>
+            Sign In
+          </button>
+          <button className='btn btn-md lg:px-[30] bg-orange-100 border border-orange text-orange font-medium text-sm lg:text-base hover:bg-orange-200 hover:text-white transition'>
+          Sign Up
+          </button>
+        </div>
+        {/* navMobile */}
+        <NavMobile/>
+      </div>
     </header>
   );
 };
